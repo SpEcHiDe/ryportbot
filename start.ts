40d822677eadd630ce29d635f0e180c6563d2f96
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2024-01-01 01:01:44 +0530

    code

--- start.ts
+++ start.ts
@@ -9,6 +9,9 @@ const TG_API_ID = parseInt(Deno.env.get("API_ID") || "0");
 const TG_API_HASH = Deno.env.get("API_HASH");
 const TG_BOT_TOKEN = Deno.env.get("BOT_TOKEN");
 
+const TRIGGER_WORD = Deno.env.get("TRIGGER_WORD") || "@admin";
+
+const ADMIN_CHAT_ID = parseInt(Deno.env.get("ADMIN_CHAT_ID") || "0");
 
 import { Client, StorageLocalStorage } from "mtkruto/mod.ts";
 
@@ -19,6 +22,10 @@ const botClient = new Client(
     TG_API_HASH,
 );
 await botClient.start(TG_BOT_TOKEN);
+
+const botMe = await botClient.getMe();
+
+console.log(`Logged in as ${botMe.username}`);
 
 console.log("Starting User Client");
 const usrClient = new Client(
@@ -32,11 +39,50 @@ await usrClient.start({
     code: () => prompt("Enter the code you received:")!,
     password: () => prompt("Enter your account's password:")!,
 });
+const usrMe = await usrClient.getMe();
+
+console.log(`Logged in as ${JSON.stringify(usrMe)}`);
 
 console.log("Started.");
 
+let TARGET_CHAT_IDS: Array<number> = [];
+const TARGET_CHAT_IDS_ = (
+    Deno.env.get("TARGET_CHAT_IDS") || ""
+).split(" ");
+for (const c of TARGET_CHAT_IDS_) {
+    if (c.startsWith("@")) {
+        const t = await usrClient.getChat(c);
+        TARGET_CHAT_IDS.push(t.id);
+    } else {
+        TARGET_CHAT_IDS.push(
+            parseInt(c),
+        );
+    }
+}
+console.log(
+    `Listening for ${TRIGGER_WORD} in ${JSON.stringify(TARGET_CHAT_IDS)}`,
+);
 
-// await usrClient.disconnect();
-// await botClient.disconnect();
-
-// console.log("Stopped");
+usrClient.on("message", async (ctx) => {
+    if (
+        TARGET_CHAT_IDS.indexOf(ctx.chat.id) > -1 &&
+        ctx.message.text.indexOf(TRIGGER_WORD) > -1
+    ) {
+        await botClient.sendMessage(
+            ADMIN_CHAT_ID,
+            "MESSAGE",
+            {
+                replyMarkup: {
+                    inlineKeyboard: [
+                        [
+                            {
+                                text: "GoTo Message",
+                                url: ctx.msg.link,
+                            },
+                        ],
+                    ],
+                },
+            },
+        );
+    }
+});
