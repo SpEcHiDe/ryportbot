3c91f4a9fe7a977435f139aa2fc265ca6c17da0b
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2024-01-01 01:29:36 +0530

    rc and lint

--- start.ts
+++ start.ts
@@ -45,7 +45,7 @@ console.log(`Logged in as ${JSON.stringify(usrMe)}`);
 
 console.log("Started.");
 
-let TARGET_CHAT_IDS: Array<number> = [];
+const TARGET_CHAT_IDS: Array<number> = [];
 const TARGET_CHAT_IDS_ = (
     Deno.env.get("TARGET_CHAT_IDS") || ""
 ).split(" ");
@@ -63,26 +63,29 @@ console.log(
     `Listening for ${TRIGGER_WORD} in ${JSON.stringify(TARGET_CHAT_IDS)}`,
 );
 
-usrClient.on("message", async (ctx) => {
-    if (
-        TARGET_CHAT_IDS.indexOf(ctx.chat.id) > -1 &&
-        ctx.message.text.indexOf(TRIGGER_WORD) > -1
-    ) {
-        await botClient.sendMessage(
-            ADMIN_CHAT_ID,
-            Deno.env.get("BOT_REPORT_MESSAGE") || "MESSAGE",
-            {
-                replyMarkup: {
-                    inlineKeyboard: [
-                        [
-                            {
-                                text: "GoTo Message",
-                                url: ctx.msg.link,
-                            },
+usrClient.on(
+    ["message", "text"],
+    async (ctx) => {
+        if (
+            TARGET_CHAT_IDS.indexOf(ctx.chat.id) > -1 &&
+            ctx.message.text.indexOf(TRIGGER_WORD) > -1
+        ) {
+            await botClient.sendMessage(
+                ADMIN_CHAT_ID,
+                Deno.env.get("BOT_REPORT_MESSAGE") || "MESSAGE",
+                {
+                    replyMarkup: {
+                        inlineKeyboard: [
+                            [
+                                {
+                                    text: "GoTo Message",
+                                    url: (ctx.message.link as string),
+                                },
+                            ],
                         ],
-                    ],
+                    },
                 },
-            },
-        );
-    }
-});
+            );
+        }
+    },
+);
